74369a4c9d4c97d1c861d3c5513eceb6dfdeeb97
air <air@MacBook-Air-air.local>
2019-11-19 15:29:12 +0100

    qPoland

--- about.html.php
+++ about.html.php
@@ -16,7 +16,7 @@
                 <h2>About Me</h2>
 
                 <p style="text-align: justify;text-justify: inter-word;">Sebastian Zajac earned his PhD in Physics at the University of Silesia in Katowice. He is currently a theoretical physicist, an assistant professor at the
-                    <a href="http://kolegia.sgh.waw.pl/en/KAE/structure/ISiD/structure/ZAHZiAW/Pages/default.aspx" target="_blank">Collegium of Economic Analysis</a>, Event History and Multilevel Analysis Unit at Warsaw School of Economics (Poland). He is a American Physical Society member.
+                    <a href="http://kolegia.sgh.waw.pl/en/KAE/structure/ISiD/structure/ZAHZiAW/Pages/default.aspx" target="_blank">Collegium of Economic Analysis</a>, Event History and Multilevel Analysis Unit at Warsaw School of Economics (Poland). He is a American Physical Society member and <a href="http://qworld.lu.lv/index.php/qpoland/">QPoland</a> member.
                     The focus of his research are Real-Time analytics, mathematical modeling in machine learning and deep learning, mathematical physics, in particular symmetries in neutrino physics and geometry, topology and Category Theory in Quantum
                     Mechanics, Quantum Field Theory adn Quantum Computing. His latest publications center around the flavour problem in the field of neutrino physics. With
                     <a href="http://psulkows.fuw.edu.pl/" target="_blank"> Piotr Sulkowski</a> they are trying to characterize RNA and protein structures by their topological characteristics called genus. The results of this work you can see on web serwer <a href="http://genus.fuw.edu.pl" target="_blank">GENUS</a>.</p>
